15c97a607b615afc7758bac06222a96fbdec744c
Glen Turner <gturner0316@yahoo.com>
2017-05-01 17:22:34 -0400

    developing logic for burger

---
+++ models/burger.js
@@ -0,0 +1,23 @@
+// Import the ORM to create functions that will interact with the database.
+
+var orm = require('../config/orm.js');
+
+var burgers = {
+    all: function(cb){
+        orm.all('burgers', function(res){
+            cb(res);
+        });
+    },
+    create: function(col, vals, cb){
+        orm.create('burgers', col, vals, function(res){
+            cb(res);
+        });
+    },
+    update: function(objColVals, condition, cb){
+        orm.update('burgers', objColVals, condition, function(res){
+            cb(res);
+        });
+    }
+};
+
+module.exports = burgers;
