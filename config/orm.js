76389872a4a14388dacf8d073d16bec5e880615d
Glen Turner <gturner0316@yahoo.com>
2017-05-02 16:10:57 -0400

    add delete function in orm

--- config/orm.js
+++ config/orm.js
@@ -57,7 +57,21 @@ var orm = {
             if(err) throw err;
             cb(result);
         });
+    },
+    delete: function (table, condition, cb){
+        var queryString = 'DELETE FROM ' + table;
+        queryString = queryString + 'WHERE';
+        queryString =  queryString + condition;
+
+        connection.query(queryString, function(err, result){
+            if (err) throw err;
+            cb(result);
+
+        });
     }
+
 };
 
 module.exports=orm;
+
+
